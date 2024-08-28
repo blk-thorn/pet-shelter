e7892cb524d70faa54fe498efa5d6fe5dd416258
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-08-28 22:16:13 +0300

    fix: remove .noscroll

--- shelter/index.js
+++ shelter/index.js
@@ -16,29 +16,29 @@ const body = document.body;
 
 if (menu && menuBtn) {
 	menuBtn.addEventListener('click', e => {
-		menu.classList.toggle('menu__list--active')
-		menuBtn.classList.toggle('burger--active')
-		body.classList.toggle('noscroll')
+		menu.classList.toggle('menu__list--active');
+		menuBtn.classList.toggle('burger--active');
+		body.classList.toggle('noscroll');
 	})
 
 	menu.addEventListener('click', e => {
 		if (e.target.classList.contains('menu__item-link')) {
-			menu.classList.remove('menu__list--active')
-			menuBtn.classList.remove('burger--active')
-			body.classList.remove('noscroll')
+			menu.classList.remove('menu__list--active');
+			menuBtn.classList.remove('burger--active');
+			body.classList.remove('noscroll');
 		}
 	})
 
 	menu.querySelectorAll('.menu__item-link').forEach(link => {
 		link.addEventListener('click', (event) => {
-			menu.classList.remove('menu__list--active')
-			menuBtn.classList.remove('burger--active')
-			body.classList.remove('noscroll')
+			menu.classList.remove('menu__list--active');
+			menuBtn.classList.remove('burger--active');
+			body.classList.remove('noscroll');
             event._isClicked = true;
             console.log("Клик по ссылке");
 		})
 	})
-}
+};
 
 
 body.addEventListener('click', (event) => {
@@ -48,9 +48,10 @@ body.addEventListener('click', (event) => {
      event.target.classList.contains('menu__item-link') == true  ||
      event.target.classList.contains('burger') == true ||
      event.target.classList.contains('burger__line') == true
-    ) return
-    console.log("Клик вне блока")
+    ) return;
+    console.log("Клик вне блока");
 
    menu.classList.remove('menu__list--active');
    menuBtn.classList.remove('burger--active');
-})
+   body.classList.remove('noscroll');
+});
