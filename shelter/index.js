fa2095346c3d1908b2e8c1afaddfd36cffdd0aaa
Victoria Ternovaya <victoria.ternovaya@gmail.com>
2024-09-08 18:00:30 +0300

    feat: add modal window on a main page

--- shelter/index.js
+++ shelter/index.js
@@ -78,6 +78,7 @@ body.addEventListener('click', (event) => {
  function cardTemplate(array, i) {
      const card = document.createElement("li");
      card.classList.add("slider__card");
+     card.setAttribute("data-modal-btn", i);
  
      const img = document.createElement("img");
      img.src = array[i].img;
@@ -90,10 +91,91 @@ body.addEventListener('click', (event) => {
      const button = document.createElement("button");
      button.classList.add("card__btn");
      button.innerText = "Learn more";
- 
+
+// ________________________modal_____________________________
+     const modal = document.createElement("div");
+     modal.classList.add("modal");
+     modal.setAttribute("data-modal-window", i);
+
+     const modalWrapper = document.createElement("div");
+     modalWrapper.classList.add("modal__wrapper")
+
+     const modalContent = document.createElement("div");
+     modalContent.classList.add("modal__content");
+
+     const modalInner = document.createElement("div");
+     modalInner.classList.add("modal__inner");
+
+     const modalInnerText = document.createElement("div");
+     modalInnerText.classList.add("modal__text");
+
+     const modalImg = document.createElement("img");
+     modalImg.classList.add("modal__img");
+     modalImg.src = array[i].img;
+
+     const modalName = document.createElement("h3");
+     modalName.classList.add("modal__title");
+     modalName.textContent = array[i].name;
+
+     const modalBreed = document.createElement("h4");
+     modalBreed.classList.add("modal__subtitle");
+     modalBreed.innerHTML = "<strong>" + array[i].type + ":</strong> " + array[i].breed;
+
+     const modalText = document.createElement("p");
+     modalText.classList.add("modal__description");
+     modalText.innerHTML = array[i].description;
+
+     const modalList = document.createElement("ul");
+     modalList.classList.add("modal__list");
+
+     const liEl = document.createElement("li");
+     liEl.classList.add("modal__item");
+     liEl.innerHTML = "<strong>Age:</strong> " + array[i].age;
+
+     const liEl2 = document.createElement("li");
+     liEl2.classList.add("modal__item");
+     liEl2.innerHTML = "<strong>Inoculations:</strong> " + array[i].inoculations;
+
+     const liEl3 = document.createElement("li");
+     liEl3.classList.add("modal__item");
+     liEl3.innerHTML = "<strong>Diseases:</strong> " + array[i].diseases;
+
+     const liEl4 = document.createElement("li");
+     liEl4.classList.add("modal__item");
+     liEl4.innerHTML = "<strong>Parasites:</strong> " + array[i].parasites;
+
+     const modalButton = document.createElement("button");
+     modalButton.classList.add("modal__button");
+     modalButton.textContent = "\u00D7";
+
+
+     card.appendChild(modal);
+     modal.appendChild(modalWrapper);
+     modalWrapper.appendChild(modalContent);
+     
+     modalContent.appendChild(modalInner);
+     modalContent.appendChild(modalInnerText);
+
+     modalWrapper.appendChild(modalButton);
+
+     modalList.appendChild(liEl);
+     modalList.appendChild(liEl2);
+     modalList.appendChild(liEl3);
+     modalList.appendChild(liEl4);
+
+
+     modalInnerText.appendChild(modalName);
+     modalInnerText.appendChild(modalBreed);
+     modalInnerText.appendChild(modalText);
+     modalInnerText.appendChild(modalList);
+
+     modalInner.appendChild(modalImg);
+
+
      card.appendChild(img);
      card.appendChild(name);
      card.appendChild(button);
+     
  
      return card;
  }
@@ -114,6 +196,16 @@ body.addEventListener('click', (event) => {
      }
  }
  
+//  function addUniqueCards(container, count, usedIndices) {
+//     while (usedIndices.size < count) {
+//         const index = Math.floor(Math.random() * shuffledArr.length);
+//         if (!usedIndices.has(index)) {
+//             usedIndices.add(index);
+//             const newCard = cardTemplate(shuffledArr, index);
+//             container.appendChild(newCard);
+//         }
+//     }
+// }
 
  addUniqueCards(activeCards, 3);
  addUniqueCards(leftCards, 3);
@@ -122,7 +214,7 @@ body.addEventListener('click', (event) => {
 
 
     const sliderWrapper = document.querySelector(".slider__wrapper")
-    const cards = document.querySelectorAll(".slider__card");
+    const cards = document.querySelectorAll(".slider__card").innerHTML;
     const prevBtn = document.querySelector(".slider__btn--left");
     const nextBtn = document.querySelector(".slider__btn--right");
 
@@ -142,34 +234,68 @@ body.addEventListener('click', (event) => {
     nextBtn.addEventListener('click', moveRight);
 
 
+    // const usedLeftIndices = new Set();
+    // const usedRightIndices = new Set();
+    
     slider.addEventListener("animationend", (animationEvent) => {
-
         let changedCards;
-
+    
         if (animationEvent.animationName === "move-left") {
             slider.classList.remove("transition-left");
             changedCards = leftCards;
             activeCards.innerHTML = changedCards.innerHTML;
-
+    
             changedCards.innerHTML = "";
-
+    
             addUniqueCards(leftCards, 3);
-
-   
+            // addUniqueCards(leftCards, 3, usedLeftIndices);
+    
         } else {
             slider.classList.remove("transition-right");
             changedCards = rightCards;
             activeCards.innerHTML = changedCards.innerHTML;
-
+    
             changedCards.innerHTML = "";
-
+    
             addUniqueCards(rightCards, 3);
-        };
+            // addUniqueCards(rightCards, 3, usedRightIndices);
+        }
+    
         prevBtn.addEventListener('click', moveLeft);
         nextBtn.addEventListener('click', moveRight);
+    });
+    
+
+    
+        const modalContainer = document.querySelectorAll(".slider__card");
+        modalContainer.forEach(card => {
+            card.addEventListener("click", (event) => {
+                const target = event.target;
+
+                const modalBtnElement = target.closest("[data-modal-btn]");
+
+                const name = modalBtnElement.dataset.modalBtn;
+
+                const modal = document.querySelector(`[data-modal-window='${name}']`);
+    
+                    modal.style.display = "flex";
+                    document.body.style.overflow = "hidden";
+    
+            });
         });
 
 
 
-
-        
+        window.onclick = function (e) {
+            if (
+                e.target.classList.contains("modal__wrapper")||
+                e.target.classList.contains("modal__button")||
+                e.target.hasAttribute("data-modal-window")) {
+                
+                let modals = document.querySelectorAll("*[data-modal-window]");
+                for (let i = 0; i < modals.length; i++) {
+                    modals[i].style.display = "none";
+                }
+                document.body.style.overflow = "auto";
+            }
+        };
